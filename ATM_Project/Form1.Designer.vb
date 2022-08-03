b842bba8e0aadab853c13e16647c5a38b4df5f95
DeadlyWind <110469858+DeadlyWind@users.noreply.github.com>
2022-08-03 22:57:20 +0530

    Add files via upload

--- ATM_Project/Form1.Designer.vb
+++ ATM_Project/Form1.Designer.vb
@@ -23,43 +23,43 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
+        Me.IDLabel = New System.Windows.Forms.Label()
+        Me.IdBox = New System.Windows.Forms.TextBox()
+        Me.PasswordLabel = New System.Windows.Forms.Label()
         Me.PasswordBox = New System.Windows.Forms.TextBox()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RobotCheck = New System.Windows.Forms.CheckBox()
+        Me.SubmitButton = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Label1
+        'IDLabel
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.BackColor = System.Drawing.SystemColors.ControlLight
-        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(156, 132)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(100, 26)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Login Id"
+        Me.IDLabel.AutoSize = True
+        Me.IDLabel.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.IDLabel.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.IDLabel.Location = New System.Drawing.Point(156, 132)
+        Me.IDLabel.Name = "IDLabel"
+        Me.IDLabel.Size = New System.Drawing.Size(100, 26)
+        Me.IDLabel.TabIndex = 0
+        Me.IDLabel.Text = "Login Id"
         '
-        'TextBox1
+        'IdBox
         '
-        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
-        Me.TextBox1.Location = New System.Drawing.Point(305, 130)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(319, 31)
-        Me.TextBox1.TabIndex = 1
+        Me.IdBox.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.IdBox.Location = New System.Drawing.Point(305, 130)
+        Me.IdBox.Name = "IdBox"
+        Me.IdBox.Size = New System.Drawing.Size(319, 31)
+        Me.IdBox.TabIndex = 1
         '
-        'Label2
+        'PasswordLabel
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.BackColor = System.Drawing.SystemColors.ControlLight
-        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label2.Location = New System.Drawing.Point(156, 206)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(110, 26)
-        Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Password"
+        Me.PasswordLabel.AutoSize = True
+        Me.PasswordLabel.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.PasswordLabel.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.PasswordLabel.Location = New System.Drawing.Point(156, 206)
+        Me.PasswordLabel.Name = "PasswordLabel"
+        Me.PasswordLabel.Size = New System.Drawing.Size(110, 26)
+        Me.PasswordLabel.TabIndex = 2
+        Me.PasswordLabel.Text = "Password"
         '
         'PasswordBox
         '
@@ -69,29 +69,29 @@ Partial Class Form1
         Me.PasswordBox.Size = New System.Drawing.Size(319, 31)
         Me.PasswordBox.TabIndex = 3
         '
-        'CheckBox1
+        'RobotCheck
         '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.BackColor = System.Drawing.Color.CadetBlue
-        Me.CheckBox1.Font = New System.Drawing.Font("Times New Roman", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.CheckBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.CheckBox1.Location = New System.Drawing.Point(305, 281)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(197, 29)
-        Me.CheckBox1.TabIndex = 4
-        Me.CheckBox1.Text = "I am not a Robot"
-        Me.CheckBox1.UseVisualStyleBackColor = False
+        Me.RobotCheck.AutoSize = True
+        Me.RobotCheck.BackColor = System.Drawing.Color.CadetBlue
+        Me.RobotCheck.Font = New System.Drawing.Font("Times New Roman", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.RobotCheck.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.RobotCheck.Location = New System.Drawing.Point(305, 281)
+        Me.RobotCheck.Name = "RobotCheck"
+        Me.RobotCheck.Size = New System.Drawing.Size(197, 29)
+        Me.RobotCheck.TabIndex = 4
+        Me.RobotCheck.Text = "I am not a Robot"
+        Me.RobotCheck.UseVisualStyleBackColor = False
         '
-        'Button1
+        'SubmitButton
         '
-        Me.Button1.BackColor = System.Drawing.Color.DarkTurquoise
-        Me.Button1.Font = New System.Drawing.Font("Times New Roman", 11.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
-        Me.Button1.Location = New System.Drawing.Point(476, 369)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(217, 50)
-        Me.Button1.TabIndex = 5
-        Me.Button1.Text = "Submit"
-        Me.Button1.UseVisualStyleBackColor = False
+        Me.SubmitButton.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.SubmitButton.Font = New System.Drawing.Font("Times New Roman", 11.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.SubmitButton.Location = New System.Drawing.Point(476, 369)
+        Me.SubmitButton.Name = "SubmitButton"
+        Me.SubmitButton.Size = New System.Drawing.Size(217, 50)
+        Me.SubmitButton.TabIndex = 5
+        Me.SubmitButton.Text = "Submit"
+        Me.SubmitButton.UseVisualStyleBackColor = False
         '
         'Form1
         '
@@ -99,23 +99,23 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.SubmitButton)
+        Me.Controls.Add(Me.RobotCheck)
         Me.Controls.Add(Me.PasswordBox)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PasswordLabel)
+        Me.Controls.Add(Me.IdBox)
+        Me.Controls.Add(Me.IDLabel)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Login Page"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Label1 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Label2 As Label
+    Friend WithEvents IDLabel As Label
+    Friend WithEvents IdBox As TextBox
+    Friend WithEvents PasswordLabel As Label
     Friend WithEvents PasswordBox As TextBox
-    Friend WithEvents CheckBox1 As CheckBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents RobotCheck As CheckBox
+    Friend WithEvents SubmitButton As Button
 End Class
--- ATM_Project/Form1.vb
+++ ATM_Project/Form1.vb
@@ -1,9 +1,9 @@
 ﻿Public Class Form1
-    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles RobotCheck.CheckedChanged
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
 
     End Sub
 End Class
