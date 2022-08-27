0d01afaa375f17172d4219006be61bb5f22a3fe6
Sunit Mal <110469858+sunit-mal@users.noreply.github.com>
2022-08-28 02:39:14 +0530

    Delete ATM_Project directory

--- ATM_Project/Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles RobotCheck.CheckedChanged
-
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
-
-    End Sub
-End Class
