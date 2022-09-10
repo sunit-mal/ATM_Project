f4da5555bf43e10676f8e3ed48ca7dd622498c82
xerosRupam <110549233+xerosRupam@users.noreply.github.com>
2022-09-11 02:50:27 +0530

    Add files via upload

---
+++ sign in.py
@@ -0,0 +1,18 @@
+class User:
+    def Money():
+        import tkinter as tkk
+
+        root=tkk.Tk()
+        root.title("User Type")
+        root.geometry("1280x720")
+        root.configure(bg='lightblue')
+        root.resizable(False, False)
+        root.iconbitmap('./download.ico')
+
+        tkk.Button(root,text="New User", height=3, width=50, bd=5, font=("Helvetica")).place(x=140,y=320)
+        tkk.Button(root,text="Existing User", height=3, width=50, bd=5, font=("Helvetica")).place(x=700,y=320)
+       
+
+        tkk.mainloop()
+
+User.Money()
