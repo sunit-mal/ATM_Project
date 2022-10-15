3c471b6645376daaa8be4f50cf53bde88f099dc3
xerosRupam <110549233+xerosRupam@users.noreply.github.com>
2022-10-16 02:09:40 +0530

    Add files via upload

--- SignType.py
+++ SignType.py
@@ -4,17 +4,25 @@ class User:
     def Run():
         import tkinter as tkk
 
+        def ExsistingUser():
+            root.destroy()
+            from Login import LoginPageForExsistingUser as p
+            p.Run()
+        def NewUser():
+            root.destroy()
+            from Login import LoginPageForNewUser as o
+            o.Run()
         root=tkk.Tk()
         root.title("User Type")
         root.geometry("1280x720")
         root.configure(bg='lightblue')
         root.resizable(False, False)
-        root.iconbitmap('./download.ico')
+        root.iconbitmap('D:\Codes\Software Project\ATM_Project-Main\download.ico')
 
-        tkk.Button(root,text="New User", height=3, width=50, bd=5, font=("Helvetica")).place(x=140,y=320)
-        tkk.Button(root,text="Existing User", height=3, width=50, bd=5, font=("Helvetica")).place(x=700,y=320)
-       
+        tkk.Button(root,text="New User", height=3, width=50, bd=5, font=("Helvetica"),command=NewUser).place(x=140,y=320)
+        tkk.Button(root,text="Existing User", height=3, width=50, bd=5, font=("Helvetica"),command=ExsistingUser).place(x=700,y=320)
+    
 
         tkk.mainloop()
 
-User.Run()
+# User.Run()
