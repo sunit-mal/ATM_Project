d9b3b4f675dfccf2ee26fc2c0ae04fdfddfdec34
Sunit Mal <110469858+sunit-mal@users.noreply.github.com>
2022-10-16 01:40:08 +0530

    Add files via upload

--- WithdrawlPage.py
+++ WithdrawlPage.py
@@ -1,8 +1,31 @@
 #   Window - 7
 
 class Withdrawl:
-    def Run():
+    def Run(Usernum):
         import tkinter as tkk
+        from openpyxl import Workbook, load_workbook
+
+        def submit():
+                amountstr = amount_var.get()
+                amount = int(amountstr)
+                ws[f'F{Usernum}']=str(ws[f'F{Usernum}'].value)+';'+str(ws[f'C{Usernum}'].value)+'-'+str(amount)
+                ws[f'C{Usernum}'] = int(ws[f'C{Usernum}'].value) - amount
+                wb.save('DataBase.xlsx')
+                # amount_var.set('')
+                root.destroy()
+                from collectmoney import collectmoney as c
+                c.play()
+
+        def clean():
+            amount_var.set('')
+
+        def backHome():
+            root.destroy()
+            from SignType import User as H
+            H.Run()
+
+        wb = load_workbook('DataBase.xlsx')
+        ws = wb['BalanceSheet']   
 
         root=tkk.Tk()
         root.title("Withdrawl")
@@ -10,15 +33,16 @@ class Withdrawl:
         root.configure(bg='lightblue')
         root.resizable(False, False)
         root.iconbitmap('./download.ico')
+        amount_var = tkk.StringVar()
 
         tkk.Label(root,text="Amount:", font=("Times New Roman", 40)).place(x=200,y=153)
         
-        tkk.Entry(root,bd=4, font=("Times New Roman", 30)).place(x=600,y=153)
+        tkk.Entry(root,textvariable=amount_var,bd=4, font=("Times New Roman", 30)).place(x=600,y=153)
 
-        tkk.Button(root,text="Home", height=3, width=25, bd=5, font=("Helvetica")).place(x=145,y=560)
-        tkk.Button(root,text="Clear", height=3, width=25, bd=5, font=("Helvetica")).place(x=650,y=560)
-        tkk.Button(root,text="Next",height=3,width=25,bd=5,font=("Helvetica")).place(x=950,y=560)
+        tkk.Button(root,text="Home", height=3, width=25, bd=5, font=("Helvetica"),command=backHome).place(x=145,y=560)
+        tkk.Button(root,text="Clear", height=3, width=25, bd=5, font=("Helvetica"),command=clean).place(x=650,y=560)
+        tkk.Button(root,text="Next",height=3,width=25,bd=5,font=("Helvetica"),command=submit).place(x=950,y=560)
 
         tkk.mainloop()
 
-Withdrawl.Run()
+Withdrawl.Run(2)
